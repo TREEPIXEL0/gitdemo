fe4cf2eae0625305884eed70edcf04f1c6261f92
dtwork <dreamstodayds@163.com>
2024-06-14 12:46:41 +0800

    Revert "Reapply "This is my second commit""
    This reverts commit 8e6ed969cc2dedb5f36894de5586982f503b7f98.

--- GitDemo/Program.cs
+++ GitDemo/Program.cs
@@ -1,6 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, World!");
+
 Console.WriteLine("This is my first commit");
-Console.WriteLine("This is my second commit");
-Console.WriteLine("Three sentence");
