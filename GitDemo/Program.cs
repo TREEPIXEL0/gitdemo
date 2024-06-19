2c6239e71e1a99e209490ab54b22895c217da0ed
dtwork <dreamstodayds@163.com>
2024-06-19 10:08:05 +0800

    desktop

--- GitDemo/Program.cs
+++ GitDemo/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, World!");
-
+Console.WriteLine("desktop");
 Console.WriteLine("This is my first commit");
 Console.WriteLine("one");
 Console.WriteLine("two");
